bf8a7fa39c6e17ad1d9a6a902edacdcd2548771d
meakashdash <akashdash279@gmail.com>
2024-07-08 06:03:31 +0000

    changes made to run globally

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,4 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+#!/usr/bin/env node
 import { program } from "commander";
 import inquirer from "inquirer";
 import fs from "fs";
@@ -38,25 +30,23 @@ function displayBanner() {
         width: boxWidth
     }));
 }
-function languageSelect() {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (!config.language) {
-            displayBanner();
-            const answer = yield inquirer.prompt([
-                {
-                    type: "list",
-                    name: "language",
-                    message: "Select Language:",
-                    choices: [
-                        { name: chalk.yellow("Javascript"), value: "js" },
-                        { name: chalk.blue("TypeScript"), value: "ts" },
-                    ],
-                },
-            ]);
-            config.language = answer.language;
-            fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
-        }
-    });
+async function languageSelect() {
+    if (!config.language) {
+        displayBanner();
+        const answer = await inquirer.prompt([
+            {
+                type: "list",
+                name: "language",
+                message: "Select Language:",
+                choices: [
+                    { name: chalk.yellow("Javascript"), value: "js" },
+                    { name: chalk.blue("TypeScript"), value: "ts" },
+                ],
+            },
+        ]);
+        config.language = answer.language;
+        fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
+    }
 }
 program
     .version("1.0.0")
@@ -64,44 +54,42 @@ program
 program
     .command("generate <type> <name>")
     .description("Generate a model, controller, or router")
-    .action((type, name) => __awaiter(void 0, void 0, void 0, function* () {
-    yield languageSelect();
+    .action(async (type, name) => {
+    await languageSelect();
     if (type !== "module" &&
         (type === "model" || type === "controller" || type === "router")) {
-        yield generateFile(name, type);
+        await generateFile(name, type);
     }
     else if (type === "module") {
         console.log(chalk.cyan('🎉 Generating full module...'));
-        yield generateFile(name, "model");
-        yield generateFile(name, "controller");
-        yield generateFile(name, "router");
+        await generateFile(name, "model");
+        await generateFile(name, "controller");
+        await generateFile(name, "router");
     }
     else {
         console.log(chalk.red('❌ Invalid type. Use "model", "controller", "router", or "module".'));
     }
-}));
-function generateFile(name, type) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const folderPath = path.join(process.cwd(), name);
-        if (!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath);
-        }
-        const fileName = `${name}.${type}.${config.language}`;
-        const filePath = path.join(folderPath, fileName);
-        const relativePath = path.join(name, fileName);
-        if (!fs.existsSync(filePath)) {
-            const spinner = ora(`Creating ${type} ${name}...`).start();
-            yield new Promise(resolve => setTimeout(resolve, 1000));
-            fs.writeFileSync(filePath, yield getTemplate(name, type));
-            const stats = fs.statSync(filePath);
-            const fileSizeInBytes = stats.size;
-            const fileSizeInKB = fileSizeInBytes / 1024;
-            spinner.succeed(chalk.green(`✨ ${type.charAt(0).toUpperCase() + type.slice(1)} ${name} ${chalk.cyan(`(${relativePath}) (${fileSizeInKB.toFixed(2)} KB)`)} created successfully!`));
-        }
-        else {
-            console.log(chalk.yellow(`😅 Oops! ${type.charAt(0).toUpperCase() + type.slice(1)} ${name} already exists!`));
-        }
-    });
+});
+async function generateFile(name, type) {
+    const folderPath = path.join(process.cwd(), name);
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+    }
+    const fileName = `${name}.${type}.${config.language}`;
+    const filePath = path.join(folderPath, fileName);
+    const relativePath = path.join(name, fileName);
+    if (!fs.existsSync(filePath)) {
+        const spinner = ora(`Creating ${type} ${name}...`).start();
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        fs.writeFileSync(filePath, await getTemplate(name, type));
+        const stats = fs.statSync(filePath);
+        const fileSizeInBytes = stats.size;
+        const fileSizeInKB = fileSizeInBytes / 1024;
+        spinner.succeed(chalk.green(`✨ ${type.charAt(0).toUpperCase() + type.slice(1)} ${name} ${chalk.cyan(`(${relativePath}) (${fileSizeInKB.toFixed(2)} KB)`)} created successfully!`));
+    }
+    else {
+        console.log(chalk.yellow(`😅 Oops! ${type.charAt(0).toUpperCase() + type.slice(1)} ${name} already exists!`));
+    }
 }
 function getTemplate(name, type) {
     switch (type) {
